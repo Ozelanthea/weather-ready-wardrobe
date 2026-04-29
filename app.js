d677714f842ab30fd5cedaa64c1a45706e0542d1
Ozelanthea <ozelanthea@gmail.com>
2026-04-29 06:11:37 +0100

    Add outfit recommendaton logic with 9 weather conditions

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ const weatherDescription = document.querySelector("#weather-description");
 const humidity = document.querySelector("#humidity");
 const windSpeed = document.querySelector("#wind-speed");
 const resultsCard = document.querySelector("#results-card");
+const adviceDisplay = document.querySelector("#outfit-recommendation");
 
 async function getWeather() {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=5353309fcaa7998aa6dd6c93a676eef6`);
     const data = await response.json();
 
     console.log(data);
+    
+    const advice = getOutfitAdvice(data.main.temp, data.weather[0].main);
+
+    adviceDisplay.textContent = `Advice: ${advice}`
 
     cityName.textContent = `📍: ${data.name}`;
     
@@ -24,8 +29,36 @@ async function getWeather() {
     windSpeed.textContent = `Wind Speed: ${data.wind.speed}m/s`;
 
     resultsCard.style.display = "block";
+
+    
 }
 
 btn.addEventListener("click", (event) => {
     getWeather();
 })
+
+function getOutfitAdvice(temp, condition) {
+    if (condition === "Thunderstorm") {
+        return "Stay indoors! It's safer to watch the lightning from your window.";
+    } else if (condition === "Snow") {
+        return "Time for thermal layers, a heavy puffer coat, and boots with good grip."
+    } else if (condition === "Mist" || condition === "Fog" || condition === "Haze") {
+        return "Visibility is low today — drive carefully and wear bright colours."
+    } else if (condition === "Rain" || condition === "Drizzle") {
+        return "Carry a waterproof jacket and an umbrella. Stay dry!"
+    } else if (condition === "Clear" && temp > 30) {
+        return "It's scorching! Wear light linen clothes, a hat, and don't forget sunblock."
+    } else if (condition === "Clear" && temp >= 20 && temp <= 30) {
+        return "Perfect weather! A light t-shirt and shorts or a summer dress will be great."
+    } else if (condition === "Clouds" && temp < 20) {
+        return "A bit chilly and grey. A sweater or a light denim jacket is a safe bet."
+    } else if (condition === "Clouds" && temp >= 20) {
+        return "It's cloudy but warm! A comfortable t-shirt or long-sleeve shirt is all you need."
+    } else if (temp < 10) {
+        return "It's freezing! Grab your scarf, gloves, and a warm wool coat."
+    }  else {
+        return "Dress in comfortable layers so you can adjust throughout the day."
+    }
+}
+
+
